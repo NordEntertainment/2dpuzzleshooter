7337b4b85ad454b0e9d2bb03fc356afb9487883a
fannock <fannock@gmail.com>
2016-02-28 16:13:39 +0100

    testlevel

---
+++ 2D Puzzle Shooter/Assets/Scripts/Levels/TestLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLevel : SpawnTester {
+
+	public SpawnTester ST;
+
+	void Start () {
+
+		for (int i = 0; i < 3; i++)
+		{
+			RedCube ();
+		}
+		for (int i = 0; i < 2; i++)
+		{
+			YellowCube ();
+		}
+		for (int i = 0; i < 5; i++)
+		{
+			GreenCube ();
+		}
+	
+		
+	}
+}
