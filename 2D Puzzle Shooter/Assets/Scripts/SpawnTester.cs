d1a8887bdd6d91722ce7f539c373661bdb9dc302
fannock <fannock@gmail.com>
2016-02-28 15:22:32 +0100

    sdfdf

--- 2D Puzzle Shooter/Assets/Scripts/SpawnTester.cs
+++ 2D Puzzle Shooter/Assets/Scripts/SpawnTester.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SpawnTester : MonoBehaviour {
 
+	public GameObject Block;
+	public int maxBox = 3;
+	public int maxX = 30f;
+	public int minX = 0f;
+	public int maxY = 30f;
+	public int minY = 30f;
+
+
+	private Vector2 originPosition;
+
+
+
 	// Use this for initialization
 	void Start () {
-	
+
+		originPosition = transform.position;
+		Spawn ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Spawn()
+	{
+		for (int i = 0; i < maxBox; i++)
+		{
+			Vector2 randomPosition = originPosition + new Vector2 (Random.Range(minX, maxX), Random.Range (minY, maxY));
+			Instantiate (Block, randomPosition, Quaternion.identity);
+			originPosition = randomPosition;
+		}
 	}
+
 }
